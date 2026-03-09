08979fe12af1c9355dc4a8f794cef3836b5543e1
Eduardo Mata <eddiemata15533@gmail.com>
2026-03-09 01:33:07 -0500

    Finished Palindrome exercise

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System;
+
+namespace PalindromeExercise;
+
+public class WordSmith
+{
+    public bool IsPalindrome(string word)
+    {
+        if (string.IsNullOrEmpty(word))
+        {
+            return false;
+            
+        }
+        // get string and convert to lowercase and remove non-alphanumeric characters 
+        string normalized = new string(word.Where(char.IsLetterOrDigit).ToArray()).ToLower();
+        
+        // reverse string 
+        char[] charArray = normalized.ToCharArray();
+        Array.Reverse(charArray);
+        string reversed = new string(charArray);
+        
+        // compare original to reversed string 
+        return normalized.Equals(reversed);
+    }
+    
+}
--- PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
+++ PalindromeExercise/PalindromeExerciseTests/UnitTest1.cs
@@ -1,13 +1,25 @@
 using System;
+using PalindromeExercise;
 using Xunit;
 
 namespace PalindromeExerciseTests
 {
     public class UnitTest1
     {
-        [Fact]
-        public void Test1()
+        [Theory]
+        [InlineData("Racecar", true)]
+        [InlineData("car", false)]
+        public void Mytest(string word, bool expected)
         {
+            // arrange
+            var tester = new WordSmith();
+            
+
+            //act
+            var actual = tester.IsPalindrome(word);
+
+            //assert
+            Assert.Equal(expected, actual);
 
         }
     }
